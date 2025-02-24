ff8719954116b034efd10e4d94ebcf3406663f7c
Muaz717 <m.azizov03@mail.ru>
2025-02-24 12:04:33 +0300

    first commit

---
+++ src/main/java/com/muaz/gym/GymApp2Application.java
@@ -0,0 +1,13 @@
+package com.muaz.gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymApp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/muaz/gym/GymApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muaz.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
